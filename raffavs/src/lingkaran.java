5149530e22dea72e7b5e6f2225adaa55223ca673
Raffamustari <raffaraffirefka123@gmail.com>
2024-10-03 15:24:31 +0700

    Add files via upload

--- raffavs/src/lingkaran.java
+++ raffavs/src/lingkaran.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 
-public class lingkaran {
-    public static void main(String[] args) {
-        
-        Scanner mtk = new Scanner(System.in);
 
-        System.out.println("masukann luas lingkaran ");
+public class lingkaran {
+    public static void main(String[] args) { 
+        Scanner mtk = new Scanner(System.in);
+        
+
+        System.out.println("masukan luas lingkaran");
         double luas = mtk.nextDouble();
-        double keliling = 2 * 22/7 * luas;
-        System.out.println("keliling lingkaran = " + keliling);
+
+        double keliling = 2 * 22/7 * luas ; 
+
+        System.out.println("keliling lingkaran =" + keliling);
+
         mtk.close();
     }
-    
 }
